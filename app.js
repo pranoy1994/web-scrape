73b2424575e0c69984e828e4c27d6e64a6b5a962
Pranoy Biswas <pranoybiswas1994@gmail.com>
2017-11-16 15:30:15 +0530

    added corser module

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var corser = require("corser");
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(corser.create());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -44,10 +44,23 @@ app.use('/users', users);
 //   res.render('error');
 // });
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+// app.use(express.methodOverride());
 
+// ## CORS middleware
+// 
+// // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+// var allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+//   // intercept OPTIONS method
+//   if ('OPTIONS' == req.method) {
+//     res.send(200);
+//   }
+//   else {
+//     next();
+//   }
+// };
+// app.use(allowCrossDomain);
 module.exports = app;
